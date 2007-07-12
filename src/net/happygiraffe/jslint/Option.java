f8f3d115c315449010e3c7b4b7a7d1046d503019
Dominic Mitchell <dom@happygiraffe.net>
2007-07-12 12:13:55 +0000

    Switch to using an enum for the available options.

--- src/net/happygiraffe/jslint/Option.java
+++ src/net/happygiraffe/jslint/Option.java
@@ -1,105 +1,34 @@
 package net.happygiraffe.jslint;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 /**
  * @author dom
  * @version $Id$
  */
-public class Option {
+public enum Option {
+    ADSAFE   ("if use of some browser features should be restricted"),
+    BITWISE  ("if bitwise operators should not be allowed"),
+    BROWSER  ("if the standard browser globals should be predefined"),
+    CAP      ("if upper case HTML should be allowed"),
+    DEBUG    ("if debugger statements should be allowed"),
+    EQEQEQ   ("if === should be required"),
+    EVIL     ("if eval should be allowed"),
+    FRAGMENT ("if HTML fragments should be allowed"),
+    LAXBREAK ("if line breaks should not be checked"),
+    NOMEN    ("if names should be checked"),
+    PASSFAIL ("if the scan should stop on first error"),
+    PLUSPLUS ("if increment/decrement should not be allowed"),
+    RHINO    ("if the Rhino environment globals should be predefined"),
+    UNDEF    ("if undefined variables are errors"),
+    WHITE    ("if strict whitespace rules apply"),
+    WIDGET   ("if the Yahoo Widgets globals should be predefined");
 
-    private static final String ADSAFE = "adsafe";
+    private String description;
 
-    private static final String BITWISE = "bitwise";
-
-    private static final String BROWSER = "browser";
-
-    private static final String CAP = "cap";
-
-    private static final String DEBUG = "debug";
-
-    private static final String EQEQEQ = "eqeqeq";
-
-    private static final String EVIL = "evil";
-
-    private static final String FRAGMENT = "fragment";
-
-    private static final String LAXBREAK = "laxbreak";
-
-    private static final String NOMEN = "nomen";
-
-    private static final String PASSFAIL = "passfail";
-
-    private static final String PLUSPLUS = "plusplus";
-
-    private static final String RHINO = "rhino";
-
-    private static final String UNDEF = "undef";
-
-    private static final String WHITE = "white";
-
-    private static final String WIDGET = "widget";
-
-    private Map<String, Boolean> options = new HashMap<String, Boolean>();
-    {
-        options.put(ADSAFE, false);
-        options.put(BITWISE, false);
-        options.put(BROWSER, false);
-        options.put(CAP, false);
-        options.put(DEBUG, false);
-        options.put(EQEQEQ, false);
-        options.put(EVIL, false);
-        options.put(FRAGMENT, false);
-        options.put(LAXBREAK, false);
-        options.put(NOMEN, false);
-        options.put(PASSFAIL, false);
-        options.put(PLUSPLUS, false);
-        options.put(RHINO, false);
-        options.put(UNDEF, false);
-        options.put(WHITE, false);
-        options.put(WIDGET, false);
+    Option(String description) {
+        this.description = description;
     }
-
-    public boolean isAdsafe() {
-        return options.get(ADSAFE);
-    }
-
-    public boolean isEqeqeq() {
-        return options.get(EQEQEQ);
-    }
-
-    public void setAdsafe(boolean adsafe) {
-        options.put(ADSAFE, adsafe);
-    }
-
-    public void setEqeqeq(boolean eqeqeq) {
-        options.put(EQEQEQ, eqeqeq);
-    }
-
-    private List<String> sortedOptionNames() {
-        List<String> optionNames = new ArrayList<String>(options.keySet());
-        Collections.sort(optionNames);
-        return optionNames;
-    }
-
-    @Override
-    public String toString() {
-        StringBuffer sb = new StringBuffer();
-        sb.append("{");
-        // Use sorted list, so as to be more testable.
-        for (String name : sortedOptionNames()) {
-            if (options.get(name).booleanValue()) {
-                sb.append(name + ":true");
-                sb.append(",");
-            }
-        }
-        if (sb.charAt(sb.length() - 1) == ',')
-            sb.deleteCharAt(sb.length() - 1);
-        sb.append("}");
-        return sb.toString();
+    
+    public String getDescription() {
+        return description;
     }
 }
