2af086d611db2cde83c1bfa5aaaa3582b2d1ae07
Dominic Mitchell <dom@happygiraffe.net>
2007-07-13 12:29:49 +0000

    Better javadoc.

--- src/net/happygiraffe/jslint/Option.java
+++ src/net/happygiraffe/jslint/Option.java
@@ -5,29 +5,77 @@ package net.happygiraffe.jslint;
  * @version $Id$
  */
 public enum Option {
-    ADSAFE   ("if use of some browser features should be restricted"),
-    BITWISE  ("if bitwise operators should not be allowed"),
-    BROWSER  ("if the standard browser globals should be predefined"),
-    CAP      ("if upper case HTML should be allowed"),
-    DEBUG    ("if debugger statements should be allowed"),
-    EQEQEQ   ("if === should be required"),
-    EVIL     ("if eval should be allowed"),
-    FRAGMENT ("if HTML fragments should be allowed"),
-    LAXBREAK ("if line breaks should not be checked"),
-    NOMEN    ("if names should be checked"),
-    PASSFAIL ("if the scan should stop on first error"),
-    PLUSPLUS ("if increment/decrement should not be allowed"),
-    RHINO    ("if the Rhino environment globals should be predefined"),
-    UNDEF    ("if undefined variables are errors"),
-    WHITE    ("if strict whitespace rules apply"),
-    WIDGET   ("if the Yahoo Widgets globals should be predefined");
+    /**
+     * If use of some browser features should be restricted.
+     */
+    ADSAFE("if use of some browser features should be restricted"),
+    /**
+     * If bitwise operators should not be allowed.
+     */
+    BITWISE("if bitwise operators should not be allowed"),
+    /**
+     * If the standard browser globals should be predefined.
+     */
+    BROWSER("if the standard browser globals should be predefined"),
+    /**
+     * If upper case HTML should be allowed.
+     */
+    CAP("if upper case HTML should be allowed"),
+    /**
+     * If debugger statements should be allowed.
+     */
+    DEBUG("if debugger statements should be allowed"),
+    /**
+     * If === should be required.
+     */
+    EQEQEQ("if === should be required"),
+    /**
+     * If eval should be allowed.
+     */
+    EVIL("if eval should be allowed"),
+    /**
+     * If HTML fragments should be allowed.
+     */
+    FRAGMENT("if HTML fragments should be allowed"),
+    /**
+     * If line breaks should not be checked.
+     */
+    LAXBREAK("if line breaks should not be checked"),
+    /**
+     * If names should be checked.
+     */
+    NOMEN("if names should be checked"),
+    /**
+     * If the scan should stop on first error.
+     */
+    PASSFAIL("if the scan should stop on first error"),
+    /**
+     * If increment/decrement should not be allowed.
+     */
+    PLUSPLUS("if increment/decrement should not be allowed"),
+    /**
+     * If the Rhino environment globals should be predefined.
+     */
+    RHINO("if the Rhino environment globals should be predefined"),
+    /**
+     * If undefined variables are errors.
+     */
+    UNDEF("if undefined variables are errors"),
+    /**
+     * If strict whitespace rules apply.
+     */
+    WHITE("if strict whitespace rules apply"),
+    /**
+     * If the Yahoo Widgets globals should be predefined.
+     */
+    WIDGET("if the Yahoo Widgets globals should be predefined");
 
     private String description;
 
     Option(String description) {
         this.description = description;
     }
-    
+
     /**
      * Return a description of what this option affects.
      */
@@ -43,7 +91,7 @@ public enum Option {
     }
 
     /**
-     * Provide a JavaScript form of this option.
+     * Show this option and its description.
      */
     @Override
     public String toString() {
