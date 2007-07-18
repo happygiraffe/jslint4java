2ae8ca6e8178da00ab6d8e53f90196ec723e0d33
Dominic Mitchell <dom@happygiraffe.net>
2007-07-18 12:57:25 +0000

    Initial version of a jslint ant task.

---
+++ src/net/happygiraffe/jslint/ant/JsLintTask.java
@@ -0,0 +1,68 @@
+package net.happygiraffe.jslint.ant;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+
+import net.happygiraffe.jslint.Issue;
+import net.happygiraffe.jslint.JSLint;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.DirectoryScanner;
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.taskdefs.MatchingTask;
+
+public class JsLintTask extends MatchingTask {
+
+    private File dir;
+
+    private JSLint lint;
+
+    public void setDir(File dir) {
+        this.dir = dir;
+    }
+
+    @Override
+    public void init() throws BuildException {
+        try {
+            lint = new JSLint();
+        } catch (IOException e) {
+            throw new BuildException(e);
+        }
+    }
+
+    @Override
+    public void execute() throws BuildException {
+        if (dir == null) {
+            throw new BuildException("dir must be specified");
+        }
+
+        log("dir = " + dir, Project.MSG_DEBUG);
+
+        DirectoryScanner ds = getDirectoryScanner(dir);
+        for (String fileName : ds.getIncludedFiles()) {
+            try {
+                File file = new File(dir, fileName);
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(new FileInputStream(file)));
+                List<Issue> issues = lint.lint(file.toString(), reader);
+                if (issues.size() > 0) {
+                    for (Issue issue : issues) {
+                        log(issue.toString());
+                    }
+                }
+            } catch (FileNotFoundException e) {
+                throw new BuildException(e);
+            } catch (IOException e) {
+                throw new BuildException(e);
+            }
+        }
+
+        // Clear out for next time.
+        setDir(null);
+    }
+}
