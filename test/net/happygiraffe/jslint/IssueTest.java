13ee99d1979c2b98675fb5c0b17cfe9c96249869
Dominic Mitchell <dom@happygiraffe.net>
2007-07-30 21:55:24 +0000

    More tests for null conditions.

--- test/net/happygiraffe/jslint/IssueTest.java
+++ test/net/happygiraffe/jslint/IssueTest.java
@@ -16,16 +16,30 @@ public class IssueTest extends TestCase {
         scope = cx.initStandardObjects();
     }
 
+    @Override
+    protected void tearDown() throws Exception {
+        Context.exit();
+    }
+
+    public void testEmptyError() throws Exception {
+        Issue issue = new Issue("foo.js", scope);
+        assertEquals(null, issue.getReason());
+        assertEquals(0, issue.getLine());
+        assertEquals(0, issue.getCharacter());
+    }
+
+    public void testNullError() throws Exception {
+        Issue issue = new Issue("foo.js", null);
+        assertEquals(null, issue.getReason());
+        assertEquals(0, issue.getLine());
+        assertEquals(0, issue.getCharacter());
+    }
+
     public void testToString() {
         scope.put("reason", scope, "you broke it");
         scope.put("line", scope, 1);
         scope.put("character", scope, 1);
         Issue issue = new Issue("foo.js", scope);
         assertEquals("foo.js:1:1:you broke it", issue.toString());
-    }
-
-    @Override
-    protected void tearDown() throws Exception {
-        Context.exit();
     }
 }
