a7929db5d167770f376b8aad25fc810bcf6464b0
Dominic Mitchell <dom@happygiraffe.net>
2008-09-26 21:57:38 +0000

    Organise imports.

--- jslint4java/src/main/java/net/happygiraffe/jslint/JSLint.java
+++ jslint4java/src/main/java/net/happygiraffe/jslint/JSLint.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.Map.Entry;
 
 import org.mozilla.javascript.Context;
