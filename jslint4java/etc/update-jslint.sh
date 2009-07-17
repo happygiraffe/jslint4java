50722c1e253b99a278e422e66e9be6c343b346ec
Dominic Mitchell <dom@happygiraffe.net>
2009-07-17 23:39:28 +0100

    Chase package-rename.

--- jslint4java/etc/update-jslint.sh
+++ jslint4java/etc/update-jslint.sh
@@ -8,7 +8,7 @@ dir=${0%/*}
 
 javaDir="src/main/java"
 resourceDir="src/main/resources"
-pkgDir="net/happygiraffe/jslint"
+pkgDir="com/googlecode/jslint4java"
 
 $dir/fetch-jslint.sh $resourceDir $pkgDir
 $dir/extract-options.rb $resourceDir/$pkgDir/fulljslint.js $javaDir/$pkgDir/Option.java > tmp.java
