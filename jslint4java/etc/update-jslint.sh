8cf099d8581196c9d82a8cd394fd62328e722213
Dominic Mitchell <dom@happygiraffe.net>
2009-10-26 07:47:58 +0000

    Allow jslint to come from a file as well as an URL.
    This is because I now have it floating around as part of a local git repository.

--- jslint4java/etc/update-jslint.sh
+++ jslint4java/etc/update-jslint.sh
@@ -9,9 +9,15 @@ dir=${0%/*}
 javaDir="src/main/java"
 resourceDir="src/main/resources"
 pkgDir="com/googlecode/jslint4java"
+jslint="fulljslint.js"
 
-$dir/fetch-jslint.sh $resourceDir $pkgDir
-$dir/extract-options.rb $resourceDir/$pkgDir/fulljslint.js $javaDir/$pkgDir/Option.java > tmp.java
+# Specify an explicit jslint instead of downloading.
+if [[ -f $1 ]] ; then
+  cp $1 $resourceDir/$pkgDir/$jslint
+else
+  $dir/fetch-jslint.sh $resourceDir $pkgDir
+fi
+$dir/extract-options.rb $resourceDir/$pkgDir/$jslint $javaDir/$pkgDir/Option.java > tmp.java
 mv tmp.java $javaDir/$pkgDir/Option.java
 
 echo "Please update jslint4java-docs now!"
