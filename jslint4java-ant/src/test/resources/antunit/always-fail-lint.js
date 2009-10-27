10f45f68971834bfcfe8f6d52e4dd7dce71392cd
Dominic Mitchell <dom@happygiraffe.net>
2009-10-27 18:22:30 +0000

    issue 18: Add in a test for the @jslint attribute.

---
+++ jslint4java-ant/src/test/resources/antunit/always-fail-lint.js
@@ -0,0 +1,11 @@
+function JSLINT(source, options) {
+  JSLINT.errors = [
+    {
+      line: 1,
+      character: 1,
+      reason: "I don't like you",
+      evidence: "eWeWeWeWeWe"
+    }
+  ];
+  return false;
+}
