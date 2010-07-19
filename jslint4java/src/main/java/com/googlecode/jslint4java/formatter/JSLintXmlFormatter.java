48bbadf14684940435f825e91fb1c2049a2f98f3
Dominic Mitchell <dom@happygiraffe.net>
2010-07-19 23:27:39 +0100

    issue 36: Add a formatter for our custom XML format.
    Again, copied from the ant project.

---
+++ jslint4java/src/main/java/com/googlecode/jslint4java/formatter/JSLintXmlFormatter.java
@@ -0,0 +1,39 @@
+package com.googlecode.jslint4java.formatter;
+
+import java.util.List;
+
+import com.googlecode.jslint4java.Issue;
+import com.googlecode.jslint4java.JSLintResult;
+
+/**
+ * Convert a single {@link JSLintResult} into an XML representation.
+ *
+ * <pre>
+ *    &lt;file name=&quot;bad.js&quot;&gt;
+ *      &lt;issue line=&quot;0&quot; char=&quot;0&quot; reason=&quot;Insufficient Llamas&quot; evidence=&quot;var sheep;&quot;/&gt;
+ *    &lt;/file&gt;
+ *    &lt;file name=&quot;good.js&quot;/&gt;
+ * </pre>
+ *
+ * @author dom
+ */
+public class JSLintXmlFormatter extends XmlFormatter implements JSLintResultFormatter {
+
+    public String format(JSLintResult result) {
+        StringBuilder sb = new StringBuilder("<file");
+        sb.append(attr("name", result.getName()));
+        sb.append(">\n");
+        List<Issue> issues = result.getIssues();
+        for (Issue issue : issues) {
+            sb.append("<issue");
+            sb.append(attr("line", Integer.toString(issue.getLine())));
+            sb.append(attr("char", Integer.toString(issue.getCharacter())));
+            sb.append(attr("reason", issue.getReason()));
+            sb.append(attr("evidence", issue.getEvidence()));
+            sb.append("/>\n");
+        }
+        sb.append("</file>");
+        return sb.toString();
+    }
+
+}
