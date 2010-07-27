cf53deb274ff7f820fbdcf60178669b449dc0708
Dominic Mitchell <dom@happygiraffe.net>
2010-07-27 08:35:49 +0100

    issue 30: Add a formatter for reports.

---
+++ jslint4java/src/main/java/com/googlecode/jslint4java/formatter/ReportFormatter.java
@@ -0,0 +1,25 @@
+package com.googlecode.jslint4java.formatter;
+
+import com.googlecode.jslint4java.JSLintResult;
+
+/**
+ * Emit an HTML fragment containing JSLint's report on the input.
+ */
+public class ReportFormatter extends XmlFormatter implements JSLintResultFormatter {
+
+    public String format(JSLintResult result) {
+        String name = result.getName();
+        StringBuilder sb = new StringBuilder();
+        sb.append("<div class='file'>");
+        sb.append("<h1");
+        sb.append(attr("id", name));
+        sb.append(">");
+        sb.append(escape(name));
+        sb.append("</h1>");
+        sb.append(result.getReport());
+        sb.append("</div>"); // try to fix somewhat crappy JSLint markup.
+        sb.append("</div>"); // close the file div.
+        return sb.toString();
+    }
+
+}
