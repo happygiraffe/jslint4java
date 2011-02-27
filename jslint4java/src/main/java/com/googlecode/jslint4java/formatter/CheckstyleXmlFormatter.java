d8a003e1e026ce4c13bff9c2cec517c02aaf2620
Dominic Mitchell <dom@happygiraffe.net>
2011-02-27 22:33:30 +0000

    issue 52: pad out Checkstyle formatter.
    LGTM, but what do I know?

--- jslint4java/src/main/java/com/googlecode/jslint4java/formatter/CheckstyleXmlFormatter.java
+++ jslint4java/src/main/java/com/googlecode/jslint4java/formatter/CheckstyleXmlFormatter.java
@@ -1,5 +1,7 @@
 package com.googlecode.jslint4java.formatter;
 
+import com.googlecode.jslint4java.Issue;
+import com.googlecode.jslint4java.JSLint;
 import com.googlecode.jslint4java.JSLintResult;
 
 public class CheckstyleXmlFormatter extends XmlFormatter implements JSLintResultFormatter {
@@ -10,7 +12,21 @@ public class CheckstyleXmlFormatter extends XmlFormatter implements JSLintResult
     }
 
     public String format(JSLintResult result) {
-        return null;
+        StringBuilder sb = new StringBuilder("<file");
+        sb.append(attr("name", result.getName()));
+        sb.append(">\n");
+        for (Issue issue : result.getIssues()) {
+            sb.append("<error");
+            sb.append(attr("line", Integer.toString(issue.getLine())));
+            sb.append(attr("column", Integer.toString(issue.getCharacter())));
+            // Based on com.puppycrawl.tools.checkstyle.api.SeverityLevel.
+            sb.append(attr("severity", "warning"));
+            sb.append(attr("message", issue.getReason()));
+            sb.append(attr("source", JSLint.class.getName()));
+            sb.append("/>\n");
+        }
+        sb.append("</file>");
+        return sb.toString();
     }
 
 }
--- jslint4java/src/test/java/com/googlecode/jslint4java/formatter/CheckstyleXmlFormatterTest.java
+++ jslint4java/src/test/java/com/googlecode/jslint4java/formatter/CheckstyleXmlFormatterTest.java
@@ -3,11 +3,23 @@ package com.googlecode.jslint4java.formatter;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import org.custommonkey.xmlunit.XMLAssert;
+import org.custommonkey.xmlunit.XMLUnit;
+import org.junit.Before;
 import org.junit.Test;
+
+import com.googlecode.jslint4java.Issue;
+import com.googlecode.jslint4java.JSLintResult;
 
 public class CheckstyleXmlFormatterTest {
 
     private final JSLintResultFormatter form = new CheckstyleXmlFormatter();
+
+    @Before
+    public void setUp() {
+        // This is why you need a proper testing library…
+        XMLUnit.setIgnoreWhitespace(true);
+    }
 
     @Test
     public void shouldHaveCheckstyleFooter() {
@@ -19,4 +31,22 @@ public class CheckstyleXmlFormatterTest {
         assertThat(form.header(), is("<checkstyle>"));
     }
 
+    @Test
+    public void shouldHaveNoProblems() throws Exception {
+        JSLintResult result = new JSLintResult.ResultBuilder("hello.js").duration(0).build();
+        String expected = "<file name=\"hello.js\" />";
+        XMLAssert.assertXMLEqual(expected, form.format(result));
+    }
+
+    @Test
+    public void shouldHaveOneProblem() throws Exception {
+        String name = "bad.js";
+        Issue issue = new Issue.IssueBuilder(name, 1, 1, "this is not a daffodil").build();
+        JSLintResult result = new JSLintResult.ResultBuilder(name).addIssue(issue).build();
+        String expected = "<file name=\"bad.js\">" +
+        		"<error line='1' column='1' severity='warning' message='this is not a daffodil'" +
+        		" source='com.googlecode.jslint4java.JSLint' />" +
+        		"</file>";
+        XMLAssert.assertXMLEqual(expected, form.format(result));
+    }
 }
