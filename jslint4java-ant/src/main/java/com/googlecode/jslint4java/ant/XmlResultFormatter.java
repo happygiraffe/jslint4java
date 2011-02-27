7dd38c47c807a38365a1b5c4335b33cb994e25f0
Dominic Mitchell <dom@happygiraffe.net>
2011-02-27 22:59:57 +0000

    issue 52: refactoring.
    Smush two classes into (nearly) one!

--- jslint4java-ant/src/main/java/com/googlecode/jslint4java/ant/XmlResultFormatter.java
+++ jslint4java-ant/src/main/java/com/googlecode/jslint4java/ant/XmlResultFormatter.java
@@ -33,14 +33,22 @@ import com.googlecode.jslint4java.formatter.JSLintXmlFormatter;
  */
 public class XmlResultFormatter implements ResultFormatter {
 
-    private final JSLintResultFormatter form = new JSLintXmlFormatter();
+    private final JSLintResultFormatter form;
     private final StringBuilder sb = new StringBuilder();
     private OutputStream out;
 
+    public XmlResultFormatter() {
+        form = createFormatter();
+    }
+
+    /** Create and return the {@link JSLintResultFormatter} used by this ant result formatter. */
+    protected JSLintResultFormatter createFormatter() {
+        return new JSLintXmlFormatter();
+    }
+
     public void begin() {
-        if (out == null) {
+        if (out == null)
             throw new BuildException("must specify destFile for xml output");
-        }
         // Clear, just in case this object gets reused.
         if (sb.length() > 0) {
             sb.delete(0, sb.length() - 1);
