136e2cc1d04cbb95dff540108dbe1fa6baa6a592
Dominic Mitchell <dom@happygiraffe.net>
2012-01-23 21:32:56 +0000

    Make assertLintOutput add extra newlines.
    It's a pain having to add an empty string after each piece of failing lint output.

--- jslint4java/src/test/java/com/googlecode/jslint4java/cli/MainTest.java
+++ jslint4java/src/test/java/com/googlecode/jslint4java/cli/MainTest.java
@@ -7,33 +7,42 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Rule;
 import org.junit.Test;
 
 import com.google.common.base.Joiner;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterables;
 
 public class MainTest {
 
     private static final String NEWLINE = System.getProperty("line.separator");
     private static final String RESOURCE_PREFIX = "com/googlecode/jslint4java/";
-    private static final List<String> NO_OUTPUT = new ArrayList<String>();
+    private static final List<String> NO_OUTPUT = ImmutableList.of();
 
     @Rule
     public StdioResource stdio = new StdioResource();
 
     private final Main main = new Main();
 
-    private void assertLintOutput(int actualExit, int expectedExit, List<String> expectedStdoutLines,
-            List<String> expectedStderrLines) throws IOException, URISyntaxException {
+    /** Check that the exit value, stdout and stderr are as expected. */
+    private void assertLintOutput(int actualExit, int expectedExit, List<String> expectedStdout,
+            List<String> expectedStderr) {
         Joiner nl = Joiner.on(NEWLINE);
-        assertThat(stdio.getStdout(), is(nl.join(expectedStdoutLines)));
-        assertThat(stdio.getStderr(), is(nl.join(expectedStderrLines)));
+        assertThat(stdio.getStdout(), is(nl.join(maybeAddTrailer(expectedStdout))));
+        assertThat(stdio.getStderr(), is(nl.join(maybeAddTrailer(expectedStderr))));
         // Do this last so that we see stdout/stderr errors first.
         assertThat(actualExit, is(expectedExit));
+    }
+
+    /** All because join() won't append a trailing newline. */
+    private Iterable<String> maybeAddTrailer(List<String> lines) {
+        if (lines.isEmpty()) {
+            return lines;
+        }
+        return Iterables.concat(lines, ImmutableList.of(""));
     }
 
     private String pathTo(String js) throws URISyntaxException {
@@ -56,10 +65,9 @@ public class MainTest {
     public void testOneBad() throws IOException, URISyntaxException {
         String path = pathTo("bad.js");
 
-        List<String> expectedStdout = Arrays.asList(
+        List<String> expectedStdout = ImmutableList.of(
                 "jslint:" + path + ":1:1:'alert' was used before it was defined.",
-                "jslint:" + path + ":1:10:Expected ';' and instead saw '(end)'.",
-                "");
+                "jslint:" + path + ":1:10:Expected ';' and instead saw '(end)'.");
         int exit = runLint(path);
         assertLintOutput(exit, 1, expectedStdout, NO_OUTPUT);
     }
