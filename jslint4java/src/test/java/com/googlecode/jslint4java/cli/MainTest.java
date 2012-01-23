1533b607e6d80de53f326badbde8a48802a9c7b2
Dominic Mitchell <dom@happygiraffe.net>
2012-01-23 21:13:40 +0000

    Extract StdioResource.

--- jslint4java/src/test/java/com/googlecode/jslint4java/cli/MainTest.java
+++ jslint4java/src/test/java/com/googlecode/jslint4java/cli/MainTest.java
@@ -3,13 +3,8 @@ package com.googlecode.jslint4java.cli;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintStream;
-import java.io.UnsupportedEncodingException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -18,70 +13,10 @@ import java.util.List;
 
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExternalResource;
 
-import com.google.common.base.Charsets;
 import com.google.common.base.Joiner;
 
 public class MainTest {
-
-    /** Wrap stdio for testing. */
-    public static class StdioResource extends ExternalResource {
-        private static final String UTF_8 = "UTF-8";
-        private InputStream origIn;
-        private PrintStream origOut;
-        private PrintStream origErr;
-        private ByteArrayOutputStream stdoutStream;
-        private ByteArrayOutputStream stderrStream;
-
-        @Override
-        protected void after() {
-            if (origIn != null) {
-                System.setIn(origIn);
-            }
-            System.setOut(origOut);
-            System.setErr(origErr);
-        }
-
-        @Override
-        protected void before() throws Throwable {
-            captureStdout();
-            captureStderr();
-        }
-
-        private void captureStderr() {
-            origErr = System.err;
-            stderrStream = new ByteArrayOutputStream();
-            System.setErr(new PrintStream(stderrStream));
-        }
-
-        private void captureStdout() {
-            origOut = System.out;
-            stdoutStream = new ByteArrayOutputStream();
-            System.setOut(new PrintStream(stdoutStream));
-        }
-
-        public String getStderr() {
-            try {
-                return stderrStream.toString(UTF_8);
-            } catch (UnsupportedEncodingException e) {
-                throw new RuntimeException(e); // "should never happen"
-            }
-        }
-
-        public String getStdout() {
-            try {
-                return stdoutStream.toString(UTF_8);
-            } catch (UnsupportedEncodingException e) {
-                throw new RuntimeException(e); // "should never happen"
-            }
-        }
-
-        public void setInput(String input) {
-            origIn = System.in;
-            System.setIn(new ByteArrayInputStream(input.getBytes(Charsets.UTF_8)));
-        }
-    }
 
     private static final String RESOURCE_PREFIX = "com/googlecode/jslint4java/";
     private static final List<String> NO_OUTPUT = new ArrayList<String>();
---
+++ jslint4java/src/test/java/com/googlecode/jslint4java/cli/StdioResource.java
@@ -0,0 +1,71 @@
+package com.googlecode.jslint4java.cli;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+
+import org.junit.rules.ExternalResource;
+
+import com.google.common.base.Charsets;
+
+/**
+ * A JUnit @Rule to wrap stdio for testing.
+ */
+public class StdioResource extends ExternalResource {
+    private static final String UTF_8 = "UTF-8";
+    private InputStream origIn;
+    private PrintStream origOut;
+    private PrintStream origErr;
+    private ByteArrayOutputStream stdoutStream;
+    private ByteArrayOutputStream stderrStream;
+
+    @Override
+    protected void after() {
+        if (origIn != null) {
+            System.setIn(origIn);
+        }
+        System.setOut(origOut);
+        System.setErr(origErr);
+    }
+
+    @Override
+    protected void before() throws Throwable {
+        captureStdout();
+        captureStderr();
+    }
+
+    private void captureStderr() {
+        origErr = System.err;
+        stderrStream = new ByteArrayOutputStream();
+        System.setErr(new PrintStream(stderrStream));
+    }
+
+    private void captureStdout() {
+        origOut = System.out;
+        stdoutStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(stdoutStream));
+    }
+
+    public String getStderr() {
+        try {
+            return stderrStream.toString(UTF_8);
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e); // "should never happen"
+        }
+    }
+
+    public String getStdout() {
+        try {
+            return stdoutStream.toString(UTF_8);
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e); // "should never happen"
+        }
+    }
+
+    public void setInput(String input) {
+        origIn = System.in;
+        System.setIn(new ByteArrayInputStream(input.getBytes(Charsets.UTF_8)));
+    }
+}
